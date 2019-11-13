2ff7951ba895cf677a82f0b7005f3fbc750edfb7
ahdelron <kthday107@gmail.com>
2019-11-13 23:41:24 +0900

    이론 상으로는 맞음. core dumped 에러만 고치면 될 듯.

--- in-postfix.c
+++ in-postfix.c
@@ -6,68 +6,47 @@ int* toPostfix(int *infix, int size) {
 	Stack *operator = NULL;
 	int *postfix = (int*)malloc(sizeof(int) * size);
 	int index = 0;
-	int isParenthese = 0;
 	for(int i = 0; *(infix+i) != '\0'; i++) {
 		int c = *(infix+i);
 		if(c < 48 || c > 57) {
 			switch((char)c) {
 				case '+':
-					if(isParenthese == 0) {
+					while(operator != NULL
+						&&(operator->data == (int)'+'
+						||operator->data == (int)'-'
+						||operator->data == (int)'*'))
 						*(postfix + index++) = c;
-						continue;
-					}
-					if(!isEmpty(operator) && operator->data == (int)'*') {
-						pop_back(&operator);
-						push_back(&operator, c);
-						push_back(&operator, (int)'*');
-					} else {
-						push_back(&operator, c);
-					}
+					push_back(&operator, c);
 					break;
 				case '-':
-					if(isParenthese == 0) {
+					while(operator != NULL
+						&&(operator->data == (int)'+'
+						||operator->data == (int)'-'
+						||operator->data == (int)'*'))
 						*(postfix + index++) = c;
-						continue;
-					}
-					if(!isEmpty(operator) && operator->data == (int)'*') {
-						pop_back(&operator);
-						push_back(&operator, c);
-						push_back(&operator, (int)'*');
-					} else {
-						push_back(&operator, c);
-					}
+					push_back(&operator, c);
 					break;
 				case '*':
-					if(isParenthese == 0) {
+					while(operator != NULL && operator->data == (int)'*')
 						*(postfix + index++) = c;
-						continue;
-					}
-					push_back(&operator, (int)'*');
+					push_back(&operator, c);
 					break;
 				case '(':
-					isParenthese += 1;
-					push_back(&operator, (int)'(');
+					push_back(&operator, c);
 					break;
 				case ')':
-					isParenthese -= 1;
-					int t;
-					while((t = pop_back(&operator)) != (int)'(')  {
-						*(postfix + index++) = t;
-					}
+					while(operator->data != '(')
+						*(postfix + index++) = pop_back(&operator);
+					pop_back(&operator); // clear the garbage.
 					break;
 				default:
-					printf("you entered another opreator that we cant provide you.\n");
-					break;
+					printf("you conveyed incorrect sentence with other operators.\n");
+					exit(1);
 			}
-			push_back(&operator, c);
-		} else {
-			// infinity linked list will be implemented.
-			*(postfix + index++) = c;
 		}
 	}
-	while(isEmpty(operator) == 0) {
+	while(isEmpty(operator) == 0)
 		*(postfix + index++) = pop_back(&operator);
-	}
-	*(postfix + index) = '\0';
+	*(postfix + index) = '\0'; // Filling End point with '\0'(NULL) value.
 	return postfix;
 }
